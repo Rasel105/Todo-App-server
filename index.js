49d8ec58fe9bbf01c1639ced51d144152e66a529
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-05-19 00:52:49 +0600

    post api created

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express()
 const port = process.env.PORT || 8000;
 
-// pass: y947QvKf2af3D7B
-// user: Rasel 
-
 // middleware 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ckoi6.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const todoCollection = client.db("todo_app").collection("todolist");
+
+
+        app.post("/todolist", async (req, res) => {
+            const todolist = req.body;
+            const result = await todoCollection.insertOne(todolist);
+            res.send(result);
+        });
+
+        app.get('/todolist', async (req, res) => {
+            const query = req.query;
+            const cursor = todoCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('Hello From todoapp')
